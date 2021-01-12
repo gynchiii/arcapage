5de5edf2905654678716983d40bab243bd500830
Joey <thepatrinot@protonmail.com>
2021-01-12 17:50:45 -0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,145 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="bg-blue-grey-12">
+    <q-toolbar class="bg-primary text-black texxt q-my-md bg-blue-grey-12 ">
+      <q-btn flat round dense   @click="leftDrawerOpen = !leftDrawerOpen" icon="menu" class="q-mr-sm" />
+        <h6 class="text-center">Grupo Arca</h6> 
+      <q-space />
+
+      <q-btn-dropdown stretch flat label="Produtos">
+        <q-list>
+          <q-item-label header class="texxt text-black">Elements</q-item-label>
+          <q-item v-for="n in 3" :key="`x.${n}`" clickable v-close-popup tabindex="0">
+            <q-item-section avatar>
+              <q-avatar icon="event_seat" color="purple" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>Cadeiras</q-item-label>
+              <q-item-label caption>February 22, 2016</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" />
+            </q-item-section>
+          </q-item>
+          <q-separator inset spaced />
+          <q-item-label header class="texxt text-black">3green</q-item-label>
+          <q-item v-for="n in 3" :key="`y.${n}`" clickable v-close-popup tabindex="0">
+            <q-item-section avatar>
+              <q-avatar icon="assignment" color="purple" text-color="white" />
+            </q-item-section>
+            <q-item-section>
+              <q-item-label>All-in-one</q-item-label>
+              <q-item-label caption>February 22, 2016</q-item-label>
+            </q-item-section>
+            <q-item-section side>
+              <q-icon name="info" />
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+      <q-btn stretch flat label="Elements" />
+      <q-btn stretch flat label="3Green" />
+    </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-blue-grey-12"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+ 
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Orbitron&display=swap');
+
+.texxt{
+  font-family: 'Orbitron', sans-serif;
+
+}
+</style>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Sobre nós',
+    caption: 'Saiba quem somos',
+    icon: 'school',
+    link: 'https://quasar.dev'
+  },
+  {
+    title: 'Conheça a Elements',
+    caption: 'Venha fazer parte desse mundo',
+    icon: 'code',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Conheça a 3Green',
+    caption: 'Venha fazer parte desse mundo',
+    icon: 'filter_3',
+    link: 'https://github.com/quasarframework'
+  },
+  {
+    title: 'Discord da comunidade',
+    caption: 'Discord',
+    icon: 'chat',
+    link: 'https://chat.quasar.dev'
+  },
+  {
+    title: 'Forum',
+    caption: 'Tire suas dúvidas',
+    icon: 'record_voice_over',
+    link: 'https://forum.quasar.dev'
+  },
+  {
+    title: 'Twitter',
+    caption: '@Grupo arca',
+    icon: 'rss_feed',
+    link: 'https://twitter.quasar.dev'
+  },
+  {
+    title: 'Facebook',
+    caption: '@Grupo arca',
+    icon: 'public',
+    link: 'https://facebook.quasar.dev'
+  },
+  {
+    title: 'Instagram',
+    caption: '<3',
+    icon: 'favorite',
+    link: 'https://awesome.quasar.dev'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,269 @@
+<template>
+  <q-page class=" bg-black">
+       <div class="q-pa-none q-gutter-md">
+    <div class="row justify-between">
+
+    <q-parallax
+     src=" https://scontent.fbnu4-1.fna.fbcdn.net/v/t1.0-9/130811889_1935168946622228_7897929910126993059_o.jpg?_nc_cat=102&ccb=2&_nc_sid=e3f864&_nc_ohc=9ievON56EKUAX_Hg86s&_nc_ht=scontent.fbnu4-1.fna&oh=45f64ae02ae53e462ce1337c398649f4&oe=6024EE26"
+    >
+      <h1 class="text-white texxt">ELEMENTS</h1>
+    </q-parallax>
+    <q-parallax>
+      <template v-slot:media>
+        <img src="https://trello-attachments.s3.amazonaws.com/5ffce91b2770135b3922c9ea/5ffdee948477bc6fb8d4034c/cf461b1ebecb22789bede5cd2fd4d654/arcanum_wallpaper.jpg" >
+      </template>
+
+      <template v-slot:content="scope">
+        <div
+          class="absolute column items-center"
+          :style="{
+            opacity: 0.7 + (1 - scope.percentScrolled) * 0.2,
+            top: (scope.percentScrolled * 100) + '%',
+            left: 0,
+            right: 0
+          }"
+        >
+          <img class="q-pa-xs" src="https://trello-attachments.s3.amazonaws.com/5ffce91b2770135b3922c9ea/5ffcebca024d872460cb026d/125570f79eadbe00a13f6822c70734ea/Untitled-1.jpg" style="width: 150px; height: 150px">
+          <br>
+          <div class="text-h2 text-white texxt text-white text-center">Vai muito além de ser só uma cadeira</div>
+          
+        </div>
+      </template>
+    </q-parallax>
+
+
+    </div>
+  </div>
+  <div class="container">
+    <h1 class="texxt text-white text-center">Produtos</h1>
+  </div>
+
+    <div class="flex q-gutter-xl q-pa-xl">
+       <q-card class="my-card2 bg-grey-1">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://www.elementsgaming.com.br/system/gallerys/imgs/000/000/052/original/cadeira2.png?1583347719"
+        />
+        <q-card-actions vertical class="justify-around bg-dark">
+          <h6 class="texxt text-white">veda</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+         <q-card class="my-card2 bg-grey-1">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://www.elementsgaming.com.br/system/gallerys/imgs/000/000/052/original/cadeira2.png?1583347719"
+        />
+        <q-card-actions vertical class="justify-around bg-dark">
+          <h6 class="texxt text-white">veda</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+        <q-card class="my-card2 bg-grey-1">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://www.elementsgaming.com.br/system/gallerys/imgs/000/000/052/original/cadeira2.png?1583347719"
+        />
+        <q-card-actions vertical class="justify-around bg-dark">
+          <h6 class="texxt text-white">veda</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+         <q-card class="my-card2 bg-grey-1">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://www.elementsgaming.com.br/system/gallerys/imgs/000/000/052/original/cadeira2.png?1583347719"
+        />
+        <q-card-actions vertical class="justify-around bg-dark">
+          <h6 class="texxt text-white">veda</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+         <q-card class="my-card2 bg-grey-1">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://www.elementsgaming.com.br/system/gallerys/imgs/000/000/052/original/cadeira2.png?1583347719"
+        />
+        <q-card-actions vertical class="justify-around bg-dark">
+          <h6 class="texxt text-white">veda</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+         <q-card class="my-card2 bg-grey-1">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://www.elementsgaming.com.br/system/gallerys/imgs/000/000/052/original/cadeira2.png?1583347719"
+        />
+        <q-card-actions vertical class="justify-around bg-dark">
+          <h6 class="texxt text-white">veda</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+    </div>
+    
+    <div class="container2 flex q-pa-xl q-gutter-xl">
+       <q-card class="my-card2 bg-black">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://http2.mlstatic.com/D_NQ_NP_833204-MLB44094590005_112020-O.jpg"
+        />
+        <q-card-actions vertical class="justify-around bg-light-green-12">
+          <h6 class="texxt">magna</h6>
+          <q-btn flat round color="red" icon="add_shopping_cart" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+       <q-card class="my-card2 bg-black">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://http2.mlstatic.com/D_NQ_NP_833204-MLB44094590005_112020-O.jpg"
+        />
+        <q-card-actions vertical class="justify-around bg-light-green-12">
+          <h6 class="texxt">magna</h6>
+          <q-btn flat round color="red" icon="add_shopping_cart" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+       <q-card class="my-card2 bg-black">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://http2.mlstatic.com/D_NQ_NP_833204-MLB44094590005_112020-O.jpg"
+        />
+        <q-card-actions vertical class="justify-around bg-light-green-12">
+          <h6 class="texxt">magna</h6>
+          <q-btn flat round color="red" icon="add_shopping_cart" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+      <q-card class="my-card2 bg-black">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://http2.mlstatic.com/D_NQ_NP_833204-MLB44094590005_112020-O.jpg"
+        />
+        <q-card-actions vertical class="justify-around bg-light-green-12">
+          <h6 class="texxt">magna</h6>
+          <q-btn flat round color="red" icon="add_shopping_cart" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+        <q-card class="my-card2 bg-black">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://http2.mlstatic.com/D_NQ_NP_833204-MLB44094590005_112020-O.jpg"
+        />
+        <q-card-actions vertical class="justify-around bg-light-green-12">
+          <h6 class="texxt">magna</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+       <q-card class="my-card2 bg-black">
+      <q-card-section horizontal>
+        <q-img
+          class="col"
+          src="https://http2.mlstatic.com/D_NQ_NP_833204-MLB44094590005_112020-O.jpg"
+        />
+        <q-card-actions vertical class="justify-around bg-light-green-12">
+          <h6 class="texxt">magna</h6>
+          <q-btn flat round color="red" icon="favorite" />
+          <q-btn flat round color="accent" icon="bookmark" />
+          <q-btn flat round color="primary" icon="share" />
+        </q-card-actions>
+      </q-card-section>
+    </q-card>
+    </div>
+    <div class="aboutus texxt text-center text-blue-grey-12"> 
+  <h1> Sobre nós </h1>
+    <div class="q-pa-md q-gutter-md">
+    <div class="row justify-between">
+
+    <q-parallax
+      src="https://trello-attachments.s3.amazonaws.com/5ffce91b2770135b3922c9ea/5ffdf3a5a9dc1e0f3ecd1879/a4aea054a8b006b0199ed8a3cb6fd7b9/AS.jpg"
+    >
+      <h1 class="text-white">Só a Elements é Elements</h1>
+    </q-parallax>
+
+    </div>
+  </div>
+    <h2>A elements é conhecida como slkdjlkasdlksajdkl
+      çlsdkfçlkçdlf
+      dfksçdlfkçsladkfçlsdf
+      dçafkçasdkfçlksadlçfs
+      asdkjalksjdlasdasdsdajsda
+      sdfkjhasjkhdfkjsadhfkjshdfd
+      fdlflksdhflkjhaskdjfhkjsdfas
+      dfsdfjlkasjdflkjlskdjflkasdjflkasdf
+      sdfklsajdflkjalskdjflkjsadlkfjlskajdf
+      sdfkljsdflkçdjfklsjdlkf
+    </h2>
+    <h2>A elements é conhecida como slkdjlkasdlksajdkl
+      çlsdkfçlkçdlf
+      dfksçdlfkçsladkfçlsdf
+      dçafkçasdkfçlksadlçfs
+      asdkjalksjdlasdasdsdajsda
+      sdfkjhasjkhdfkjsadhfkjshdfd
+      fdlflksdhflkjhaskdjfhkjsdfas
+      dfsdfjlkasjdflkjlskdjflkasdjflkasdf
+      sdfklsajdflkjalskdjflkjsadlkfjlskajdf
+      sdfkljsdflkçdjfklsjdlkf
+    </h2>
+  </div>
+  </q-page>
+</template>
+
+<style>
+.my-card2{
+  width: 100%;
+  max-width: 350px;
+}
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+   data () {
+    return {
+      lorem: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+  }
+}
+</script>
