bd309b1a5d76924d98cffdbdaf101bbb47df5b05
gynchiii <thepatrinot@protonmail.com>
2021-02-10 17:54:37 -0300

    testtest

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,10 +1,10 @@
 <template>
-  <q-page class="bg-grey-10" style="overflow-x: hidden ; overflow-y: hidden " >
+  <q-page class="bg-grey-3" style="overflow-x: hidden ; overflow-y: hidden " >
      <div class="large-screen-only">
          <div class="q-pa-none">
-           <q-parallax :height="900"  class=" q-gutter-none">
+           <q-parallax :height="950"  class=" q-gutter-none">
       <template v-slot:media>
-        <video autoplay loop muted :height="1080" src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/bgele.mp4" />
+        <video autoplay muted :height="1080" loop src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/ship.mp4" />
       </template>
 
       <template v-slot:content="scope">
@@ -12,61 +12,25 @@
           class="absolute column items-center"
           :style="{
             opacity: 1 + (0.6 - scope.percentScrolled) * 5,
-            top: (scope.percentScrolled * -40) + '%',
+            top: (scope.percentScrolled * -28) + '%',
             left: 0,
             right: 0
           }"
         >
-          <img src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/Untitled-1.png" class="q-pa-md" style="width: 750px;" >
-        <div class="q-gutter-xl justify-between flex items-center">
-          <img src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/logo-world-pc.png" class="q-pa-none" style="width: 300px;" >
-          <img src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/HQ-LOGO-PNG.png" class="q-pa-none" style="width: 300px;" >
-          <img src="https://www.elementsgaming.com.br/system/seos/imgs/000/000/001/grande/logo-elements-preta.png?1583409803" class="q-pa-none" style="width: 300px;" >
-        </div>
+          <img src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/Untitled-1.png" class="q-pa-md" style="width: 550px;" >
         </div>
       </template>
     </q-parallax>
-         
-    <q-card class="my-card" flat bordered>
+     <q-card class="my-card" flat q-pa-xl constrain border>
         <q-card-section>
-          <h4 class="texxt2 text-center constrain large-screen-only text-grey-10">O Grupo Arca nasceu com um único objetivo:
-        <br>
-        <br>
-Quebrar as barreiras do impossível, do inalcançável e construir pontes que ligam os nossos parceiros aos seus objetivos e desejos.
-<br>
-
-Somos um grupo empresarial localizado em Santa Catarina, na cidade de Palhoça. <br> <br> Somos compostos por pessoas que trabalham de maneira colaborativa e apaixonada, focada dia após dia em realizar sonhos, desenvolver negócios e a tornar o mundo melhor.</h4>
-      <h5 class="texxta text-center constrainsmall small-screen-only text-grey-6">O Grupo Arca nasceu com um único objetivo:
-        <br>
-        <br>
-Quebrar as barreiras do impossível, do inalcançável e construir pontes que ligam os nossos parceiros aos seus objetivos e desejos.
-<br>
-
-Somos um grupo empresarial localizado em Santa Catarina, na cidade de Palhoça. <br> <br> Somos compostos por pessoas que trabalham de maneira colaborativa e apaixonada, focada dia após dia em realizar sonhos, desenvolver negócios e a tornar o mundo melhor.</h5>
-        </q-card-section>
-           <q-card-section>
-          <h4 class="texxt2 text-center constrain large-screen-only text-grey-10">O Grupo Arca nasceu com um único objetivo:
-        <br>
-        <br>
-Quebrar as barreiras do impossível, do inalcançável e construir pontes que ligam os nossos parceiros aos seus objetivos e desejos.
-<br>
-
-Somos um grupo empresarial localizado em Santa Catarina, na cidade de Palhoça. <br> <br> Somos compostos por pessoas que trabalham de maneira colaborativa e apaixonada, focada dia após dia em realizar sonhos, desenvolver negócios e a tornar o mundo melhor.</h4>
-      <h5 class="texxta text-center constrainsmall small-screen-only text-grey-6">O Grupo Arca nasceu com um único objetivo:
-        <br>
-        <br>
-Quebrar as barreiras do impossível, do inalcançável e construir pontes que ligam os nossos parceiros aos seus objetivos e desejos.
-<br>
-
-Somos um grupo empresarial localizado em Santa Catarina, na cidade de Palhoça. <br> <br> Somos compostos por pessoas que trabalham de maneira colaborativa e apaixonada, focada dia após dia em realizar sonhos, desenvolver negócios e a tornar o mundo melhor.</h5>
+         <h3 class="text-center texxt2 constrain q-pa-md text-blue-10 "> <hr> O GRUPO ARCA, HOJE,<br> FAZ A GERÊNCIA COMERCIAL <br> DE 3 EMPRESAS. <hr> </h3>
         </q-card-section>
       
     </q-card>
-       </div>
-     </div>
-        <q-parallax class="small-screen-only" style="width: 100%;">
+    <h1 class=" text-blue-10 texxt text-center"> São elas...  </h1>
+          <q-parallax :height="400"  class=" q-gutter-none">
       <template v-slot:media>
-        <img  src="https://www.hdwallpapers.in/download/blue_mountains_4k_2-HD.jpg">
+        <video autoplay loop muted :height="1080" src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/bghq.mp4" />
       </template>
 
       <template v-slot:content="scope">
@@ -74,31 +38,49 @@ Somos um grupo empresarial localizado em Santa Catarina, na cidade de Palhoça. 
           class="absolute column items-center"
           :style="{
             opacity: 1 + (0.6 - scope.percentScrolled) * 5,
-            top: (scope.percentScrolled * 60) + '%',
+            top: (scope.percentScrolled * 0) + '%',
             left: 0,
             right: 0
           }"
         >
-          <img src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/Untitled-1.png" style="width: 350px" >
-          <h1 class="text-center text-blue-9 texxt">A R C A </h1>
+        <div class="q-gutter-xl q-pa-xl justify-between flex items-center">
+          <img src="https://www.elementsgaming.com.br/system/seos/imgs/000/000/001/grande/logo-elements-preta.png?1583409803" class="q-pa-none" style="width: 300px;" >
+          <img src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/logo-world-pc.png" class="q-pa-none" style="width: 300px;" >
+          <img src="https://desafio.elementsgaming.com.br/wp-content/uploads/2021/02/HQ-LOGO-PNG.png" class="q-pa-none" style="width: 300px;" >
+        </div>
         </div>
       </template>
     </q-parallax>
-    <div class="flex q-pa-xl flex-center">
-          <q-btn
-      padding="xl"
-      color="grey-4 text-blue-9"
-      push
-      style="opacity: 0.7;"
-      dense
-      to="/About"
-      round
+            <h1 class="text-center texxt text-black"> E L E M E N T S  </h1>
+       <q-parallax
+    class="large-screen-only"
+    style="width: 100%;  height: 500px;"
+      src="https://wallpapercave.com/wp/wp7196356.jpg"
     >
-    <svg-icon type="mdi" :path="path"></svg-icon>
-          </q-btn>
-    </div>
-    <h5 class="texxta text-center text-blue-6"> Clique acima para saber mais </h5>
+              <img src="https://www.elementsgaming.com.br/system/seos/imgs/000/000/001/grande/logo-elements-preta.png?1583409803" class="" style="width: 500px;" >
+         
+          <q-btn to="/elements" color="purpe" class="bg-purple-2" style="opacity: 0.8;" push >
+      <div class="row items-center text-purple text-bold texxt text-h5 no-wrap">
+        <q-icon left name="done" />
+        <div class="text-center text-black">
+          Clique<br>e Conheça
+        </div>
+      </div>
+    </q-btn>
+    <br>
+    <h4 class="texxt text-dark bg-purple-5  text-center "> Cadeiras gamers </h4>
+    <h5 class="texxt text-dark bg-purple-5  text-center "> Porém... vai além disso... </h5>
+    </q-parallax>
+   
+       </div>
+     </div>
+      <div style="left: 0;">
+    <h4 class="texxt bg-grey-2 text-center text-justify text-italic q-gutter-xl  q-pa-xl constrain text-dark"> Olá, Argen aqui quem vos fala! <br> <hr> <br> O nosso propósito, na ELEMENTS, é entregar valor para as pessoas que de alguma forma tem acesso ao nosso universo!  Tudo isso através de algo que está impregnado em nosso fit, que é o Storytelling, <hr> Onde é contado através de quadrinhos e livros, que dentro delas estão incluídos valores. Valores esses que com certeza ajudam as pessoas que tem acesso a eles a melhorarem e progredirem em suas próprias vidas, por isso estamos crescendo muito e levando esse universo inteiro pra tanta gente diferente, claro que com muita qualidade em nossos produtos juntamente com a experiência que as acompanha. <hr>
+A Elements Gaming foi criada no dia 06/07/2016 pelos sócios fundadores Rafael Seifert e Bruna Rodrigues. Já nessa data se sabia que a empresa miraria só e somente o extraordinário.
+Desde o momento zero a ideia nunca foi ser apenas uma marca de cadeira gamer, e sim ser um conceito e levar uma mensagem a todos os jovens não apenas do Brasil, mas no mundo!
+Assim, hoje, apenas 4 anos depois, orgulhamo-nos de estarmos entre as marcas gamers mais influentes da América Latina. </h4>
     <div class="small-screen-only q-gutter-md q-pa-sm q-ma-md flex flex-center">
+     </div>
 
       <q-btn
       size="22px"
@@ -323,15 +305,15 @@ Aqui, você encontrará o melhor custo benefício para computadores para empresa
 .my-card{
   width: 100%;
   max-width: 100%;
-  background-image: url("https://www.hdwallpapers.in/download/blue_mountains_4k_2-HD.jpg");
+  background-color: transparent;
 
 }
 .my-card2{
   width: 100%;
   max-width: 350px;
 }
-.bgb{
-  background-image: url("https://www.hdwallpapers.in/download/blue_mountains_4k_2-HD.jpg");
+.bgs{
+  background-image: url("https://free4kwallpapers.com/uploads/originals/2020/10/14/blue-space-nebula-wallpaper.jpg");
   background-size: 175%;
 }
 </style>
